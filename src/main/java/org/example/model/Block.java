3a0be2dcd6d3c037d4dc437ec2cfa215477bd442
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-17 16:47:26 +0200

    [add] create class Block with two fields color and material

---
+++ src/main/java/org/example/model/Block.java
@@ -0,0 +1,20 @@
+package org.example.model;
+
+public class Block {
+
+    private String color;
+    private String material;
+
+    public Block(String color, String material) {
+        this.color = color;
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+}
