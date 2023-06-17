7100271925916c6288d295fcc3aa4fcb2c4e2486
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-17 19:23:45 +0200

    [add] add test to Wall methods: getBlocks, findBlockByColor, findBlocksByMaterial, count

--- src/test/java/org/example/model/WallTest.java
+++ src/test/java/org/example/model/WallTest.java
@@ -4,6 +4,9 @@ import junit.framework.TestCase;
 import org.example.exceptions.BlockNotFoundException;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Optional;
+
 public class WallTest extends TestCase {
 
     @Test
@@ -25,7 +28,7 @@ public class WallTest extends TestCase {
         testWall.addBlock(blockToAdd);
         //then
         assertEquals(1,testWall.getBlocks().size());
-        assertEquals(true,testWall.getBlocks().contains(blockToAdd));
+        assertTrue(testWall.getBlocks().contains(blockToAdd));
     }
 
     @Test
@@ -66,5 +69,84 @@ public class WallTest extends TestCase {
         }
         assertEquals(1,testWall.getBlocks().size());
 
+    }
+
+    @Test
+    public void testGetBlocks(){
+        //given
+        Wall testWall = new Wall();
+        testWall.addBlock(new Block("red","brick"));
+        testWall.addBlock(new Block("white","brick"));
+        //when
+        List<Block> result = testWall.getBlocks();
+        //then
+        assertEquals(testWall.getBlocks(), result);
+        assertEquals(testWall.getBlocks().get(0),new Block("red","brick"));
+        assertEquals(testWall.getBlocks().get(1),new Block("white","brick"));
+    }
+
+    @Test
+    public void testFindBlockByColorWhenBlockExist(){
+        //given
+        Wall testWall = new Wall();
+        testWall.addBlock(new Block("red","brick"));
+        testWall.addBlock(new Block("white","brick"));
+        //when
+        Optional<Block> result = testWall.findBlockByColor("red");
+        //then
+        assertTrue(result.isPresent());
+        assertEquals(testWall.getBlocks().get(0), result.get());
+        assertEquals(testWall.getBlocks().get(0).getColor(), "red");
+    }
+
+    @Test
+    public void testFindBlockByColorWhenBlockDontExist(){
+        //given
+        Wall testWall = new Wall();
+        testWall.addBlock(new Block("red","brick"));
+        testWall.addBlock(new Block("white","brick"));
+        //when
+        Optional<Block> result = testWall.findBlockByColor("black");
+        //then
+        assertFalse(result.isPresent());
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testFindBlocksByMaterialWhenBlockExist(){
+        //given
+        Wall testWall = new Wall();
+        testWall.addBlock(new Block("red","brick"));
+        testWall.addBlock(new Block("blue","glass"));
+        testWall.addBlock(new Block("white","brick"));
+        //when
+        List<Block> result = testWall.findBlocksByMaterial("brick");
+        //then
+        assertEquals(2,result.size());
+        assertEquals(new Block("white","brick"),result.get(1));
+    }
+
+    @Test
+    public void testFindBlocksByMaterialWhenBlockDontExist(){
+        //given
+        Wall testWall = new Wall();
+        testWall.addBlock(new Block("red","brick"));
+        testWall.addBlock(new Block("blue","glass"));
+        testWall.addBlock(new Block("white","brick"));
+        //when
+        List<Block> result = testWall.findBlocksByMaterial("plastic");
+        //then
+        assertEquals(0,result.size());
+    }
+
+    @Test
+    public void testCount(){
+        //given
+        Wall testWall = new Wall();
+        testWall.addBlock(new Block("red","brick"));
+        testWall.addBlock(new Block("blue","glass"));
+        testWall.addBlock(new Block("white","brick"));
+        //when + when
+        assertEquals(3,testWall.count());
     }
 }
